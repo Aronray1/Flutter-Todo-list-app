45331df5a6c203193d6359b1c4d47a9d2ab61217
rajbeer singh <arorakartik923@gmail.com>
2020-05-27 16:41:21 +0530

    Kartik updated

--- lib/models/task_data.dart
+++ lib/models/task_data.dart
@@ -3,53 +3,53 @@ import 'package:todo/models/task.dart';
 import 'dart:collection';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+
 final _firestore = Firestore.instance;
 List<String> data=[];
 class Taskdata extends ChangeNotifier {
  List<Task> _tasks=[
-Task(name:'data')
+
   ];
   int length;
 
 
-void addtask(String newtasktitle) async{
+void addtask(String newtasktitle) {
   try{
+
        
     _tasks.add(Task(name:newtasktitle));
     notifyListeners();
+
          
      }catch(e){
      }   // we cant update the values without this function as it auto rebuild again the widgets who are listening
                    // to this property according to it's updated value.
 }
-void getdata() async{
-  
-  await _firestore.collection("tasks").getDocuments().then((querySnapshot) {
+void getdata()async {
+   await _firestore.collection("tasks").getDocuments().then((querySnapshot) {
     querySnapshot.documents.forEach((result) {
-       _tasks.add(Task(name:result.data['title']));
+       data.add(result.data['title']);
+       
+       
     });
-
+ return data;
   });
   
-  for(var task in _tasks){
-print(task);
-  }
   
 }
-void call(){
-  getdata();
-}
+
 
 int get taskcount{
-  print('i am called taskcount');
-  return _tasks.length;
+ 
+  return data.length;
 }
 List<Task> returnlist(){
   return _tasks;
 }
 
  UnmodifiableListView<Task> get tasks {
-   print('i am called unmodifiable');
+   getdata();
+   print(taskcount);
   return UnmodifiableListView(_tasks);
 }
 void updateTask(Task task){
