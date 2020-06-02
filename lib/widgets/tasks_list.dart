6a5681d644dd14589864b7ff9ff6144a8b44aeec
rajbeer singh <arorakartik923@gmail.com>
2020-06-02 09:17:59 +0530

    Kartik updated

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -2,9 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:todo/widgets/tasks_tile.dart';  
 import 'package:provider/provider.dart';
 import 'package:todo/models/task_data.dart';
-//import 'package:provider/provider.dart';
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// var brews;
+
 class TaskList extends StatelessWidget {
  
   @override
